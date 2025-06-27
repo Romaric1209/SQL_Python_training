2d18d0e49358fcf01ac9123dadca36a1fd3081bc
Romaric Berger <romaric.berger@gmail.com>
2025-06-27 15:40:11 +0800

    learning in process

--- rental_analysis.py
+++ rental_analysis.py
@@ -2,7 +2,7 @@ import pandas as pd
 import psycopg2
 
 conn = psycopg2.connect(
-    host="local",
+    host="localhost",
     database="rental_movie_store",
     user="postgres",
     password="*"
@@ -28,3 +28,39 @@ left join customers c on r.customer_id = c.customer_id
 group by r.customer_id, name
 order by total_rental_spent desc;
 '''
+top_3c_df = pd.read_sql(top_3_customers_query, conn)
+print(top_3c_df)
+
+# Which movie genres are the most popular by number of rentals?
+top_genres_query = '''select
+m.genre as genre,
+sum(r.movie_id) as count
+from movies m
+left join rentals r on m.movie_id = r.movie_id
+group by genre
+order by count desc
+limit 3;'''
+top_genre_df = pd.read_sql(top_genres_query,conn)
+print(top_genre_df)
+
+# What is the average rental duration per genre?
+avg_rental_duration_query = '''with rental_duration as(
+select
+	r.movie_id,
+	extract(epoch from (r.return_date::timestamp - r.rental_date::timestamp)) / 86400 as duration_days
+from rentals r
+where r.return_date is not null
+)
+select
+m.genre,
+round(avg(rd.duration_days),2) as average_rental_duration
+from rental_duration rd
+left join movies m on rd.movie_id = m.movie_id
+group by m.genre
+order by average_rental_duration desc;
+'''
+avg_rental_duration_df = pd.read_sql(avg_rental_duration_query,conn)
+print(avg_rental_duration_df)
+
+conn.commit()
+conn.close()
